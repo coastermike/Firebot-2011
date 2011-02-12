18a368c42bc669f5d339551c98fcaf3abad77e0f
Michael Seppanen <coastermike@gmail.com>
2011-02-12 12:32:56 -0500

    Added config bits to code, might have figured out clock

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 #include "pins.h"
 #include "init.h"
 
+//Config Bits set here
+//Reference p24HJ128GP210A.h
+_FBS(BWRP_WRPROTECT_OFF & BSS_NO_FLASH & RBS_NO_RAM)
+_FSS(SWRP_WRPROTECT_OFF & SSS_NO_FLASH & RSS_NO_RAM)
+_FGS(GWRP_OFF & GCP_OFF & GSS_OFF)
+_FOSCSEL(FNOSC_PRI & IESO_ON)
+_FOSC(POSCMD_HS & OSCIOFNC_ON & FCKSM_CSECMD)
+_FWDT(WDTPOST_PS32768 & WDTPRE_PR128 & WINDIS_OFF & FWDTEN_OFF & PLLKEN_OFF)
+_FPOR(FPWRT_PWR128)
+_FICD(ICS_PGD2 & JTAGEN_OFF)
 
 #define On_Time  0xffff
 #define Off_Time  0xffff
@@ -14,55 +24,49 @@ int main(void)
 	int i=0;
 //	OSCCON=OSCCON | 0b0000001000000001;
 	Pin_Init();
-	PORTG = 0xF000;
+	PORTG = 0xFFFF;
 	for(i=0; i<On_Time;i++) 
 	{
-//		_asm
-//			nop
-//		_endasm
+	}
+	PORTG=0x0000;
+	for(i=0; i<On_Time;i++) 
+	{
 	}
 	while (1)
 	{
-		PORTG = 0x1000;
-			for(i=0; i<On_Time;i++) 
-	{
-//		_asm
-//			nop
-//		_endasm
-	}
-		PORTG = 0x2000;
+		PORTG=0xF000;
+//		LED1=1;
+//		LED2=0;
+//		LED3=0;
+//		LED4=0;
 	for(i=0; i<On_Time;i++) 
 	{
-//		_asm
-//			nop
-//		_endasm
 	}
-		PORTG = 0x4000;
+		PORTG=0x0000;
+	for(i=0;i<On_Time;i++)
+	{
+	}	
+/*		LED1=0;
+		LED2=1;
+		LED3=0;
+		LED4=0;
 	for(i=0; i<On_Time;i++) 
 	{
-//		_asm
-//			nop
-//		_endasm
 	}
-		PORTG = 0x8000;
+		LED1=0;
+		LED2=0;
+		LED3=1;
+		LED4=0;
 	for(i=0; i<On_Time;i++) 
 	{
-//		_asm
-//			nop
-//		_endasm
 	}
+		LED1=0;
+		LED2=0;
+		LED3=0;
+		LED4=1;
+	for(i=0; i<On_Time;i++) 
+	{
+	}*/
 
-		
-
-//		StepEN = 0;
-//		StepDIR = 0;
-//		MS1=0;
-//		do
-//		{
-//			for(i=0;i<30000;i++){LED3 =1;
-//			Step=0;}
-//			for(i=0;i<30000;i++){LED3=0;
-//			Step=1;}
-//		}while (0);	
 	}
 }	
